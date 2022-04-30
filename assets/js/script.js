f5b857585a1370df3ff59fc3f1afd39b1e454a66
SalwinA <salwino@gmail.com>
2022-04-30 12:22:29 +0000

    Add event listners to buttons and random number for calculation.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
+// Wait for the DOM to finish loading.
+// Get the button elements and add event lisitners to them.
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        });
+    }
+});
+
 function runGame() {
+    let num1 = Math.floor(Math.random() *25) + 1;
+    let num2 = Math.floor(Math.random() *25) + 1;
 
 }
 
@@ -29,3 +49,4 @@ function dispalySubtractQuestion() {
 function dispalyMultiplyQuestion() {
     
 }
+
